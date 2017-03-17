60f7f431837df4f8785b34fba33984fe7844fd33
Aaron Weidele <aweidele@r2integrated.com>
2017-03-17 15:43:58 -0400

    add thumbnails

--- roxanneweidele2017/attachment.php
+++ roxanneweidele2017/attachment.php
@@ -55,9 +55,36 @@ if(have_posts()):while(have_posts()):the_post();
     </ul>
   </nav>
 </section>
-<pre>
-<?php //print_r($gallery); ?>
-</pre>
+
+<section class="homepage-gallery">
+<?php foreach($gallery as $image) { 
+  $info = array(
+    'title' => $image['title'],
+    'mobileThumb' => $image['sizes']['Homepage Mobile Thumbnail'],
+    'thumbnail' => $image['sizes']['Homepage Thumbnail'],
+    'mobile' => $image['sizes']['Mobile Max'],
+    'xlarge' => $image['sizes']['XLarge'],
+    'large' => $image['sizes']['Large'],
+    'medium' => $image['sizes']['Med'],
+    'small' => $image['sizes']['Small'],
+  );
+?>
+  <figure>
+    <a href="<?php echo get_permalink($image["ID"]); ?>" data-slide='<?php echo json_encode($info, JSON_HEX_APOS); ?>'>
+      <picture>
+        <source srcset="<?php echo $info['mobileThumb']; ?>" media="(max-width: 667px)">
+        <source srcset="<?php echo $info['thumbnail']; ?>">
+        <img src="<?php echo $info['thumbnail']; ?>">
+      </picture>
+      <!-- img src="<?php echo $info['thumbnail']; ?>" -->
+      <figcaption>
+        <h2><?php echo $info['title']; ?></h2>
+      </figcaption>
+    </a>
+  </figure>
+<?php } ?>
+</section>
+
 <?php
 endwhile; endif;
 get_footer();
