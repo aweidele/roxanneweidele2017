d50f7a35edcc46ae5ae88005ea723896ac44db5d
Aaron Weidele <aweidele@r2integrated.com>
2017-03-20 16:57:26 -0400

    add filters to attachment page

--- roxanneweidele2017/attachment.php
+++ roxanneweidele2017/attachment.php
@@ -2,6 +2,12 @@
 get_header();
 $frontpage_id = get_option( 'page_on_front' );
 $gallery = get_field('gallery',$frontpage_id);
+
+$terms = get_terms( array(
+    'taxonomy' => 'medium',
+    'hide_empty' => false,
+) );
+
 if(have_posts()):while(have_posts()):the_post();
   
   $image = array(
@@ -55,37 +61,47 @@ if(have_posts()):while(have_posts()):the_post();
     </ul>
   </nav>
 </section>
-
+<?php endwhile; endif; ?>
 <section class="homepage-gallery">
-<?php foreach($gallery as $image) { 
+   <header>
+     <nav>
+      <ul>
+        <li><input type="radio" name="filter" value="all" id="all" checked data-title="All Works"><label for="all">All Works</label></li>
+ <?php foreach($terms as $m) { ?>
+        <li><input type="radio" name="filter" value="<?php echo $m->slug; ?>" id="<?php echo $m->slug; ?>" data-title="<?php echo $m->name; ?>"><label for="<?php echo $m->slug; ?>"><?php echo $m->name; ?></label></li>
+<?php } ?>
+      </ul>
+    </nav>
+  </header>
+  <div>
+<?php foreach($gallery as $image) {
   $info = array(
     'title' => $image['title'],
-    'mobileThumb' => $image['sizes']['Homepage Mobile Thumbnail'],
-    'thumbnail' => $image['sizes']['Homepage Thumbnail'],
-    'mobile' => $image['sizes']['Mobile Max'],
-    'xlarge' => $image['sizes']['XLarge'],
-    'large' => $image['sizes']['Large'],
-    'medium' => $image['sizes']['Med'],
-    'small' => $image['sizes']['Small'],
+    'images' => array(
+      'mobileThumb' => $image['sizes']['Homepage Mobile Thumbnail'],
+      'thumbnail' => $image['sizes']['Homepage Thumbnail'],
+      'mobile' => $image['sizes']['Mobile Max'],
+      'xlarge' => $image['sizes']['XLarge'],
+      'large' => $image['sizes']['Large'],
+      'medium' => $image['sizes']['Med'],
+      'small' => $image['sizes']['Small'],
+    ),
+    'medium' => get_field('medium',$image["ID"])
   );
 ?>
-  <figure>
+  <figure data-medium='<?php echo $info['medium']->slug; ?>'>
     <a href="<?php echo get_permalink($image["ID"]); ?>" data-slide='<?php echo json_encode($info, JSON_HEX_APOS); ?>'>
       <picture>
-        <source srcset="<?php echo $info['mobileThumb']; ?>" media="(max-width: 667px)">
-        <source srcset="<?php echo $info['thumbnail']; ?>">
-        <img src="<?php echo $info['thumbnail']; ?>">
+        <source srcset="<?php echo $info['images']['mobileThumb']; ?>" media="(max-width: 667px)">
+        <source srcset="<?php echo $info['images']['thumbnail']; ?>">
+        <img src="<?php echo $info['images']['thumbnail']; ?>">
       </picture>
-      <!-- img src="<?php echo $info['thumbnail']; ?>" -->
       <figcaption>
         <h2><?php echo $info['title']; ?></h2>
       </figcaption>
     </a>
   </figure>
 <?php } ?>
+  </div>
 </section>
-
-<?php
-endwhile; endif;
-get_footer();
-?>
+<?php get_footer(); ?>
