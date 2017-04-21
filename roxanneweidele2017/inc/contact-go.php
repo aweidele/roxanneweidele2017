e9946195e929497425066df64e3d8ee31b80e83f
Aaron Weidele <aweidele@r2integrated.com>
2017-04-21 09:00:15 -0400

    Don't send email if form was blank

--- roxanneweidele2017/inc/contact-go.php
+++ roxanneweidele2017/inc/contact-go.php
@@ -2,13 +2,14 @@
   $to = 'aweidele@gmail.com';
   $subject = $_POST['subject'];
   $message = $_POST['message'];
-  $message .= "\n\n___________________________\n\nSent via roxanneweidele.com";
+  $message .= "\n\n___________________________\n\nSent via roxanneweidele.com\n\n";
+  $message .= "Catch: ".$_POST['url'];
 
   $headers = "From: ".$_POST['name']." <".$_POST['email'].">\r\n";
   $headers .= "Reply-To: ".$_POST['email']."\r\n";
   $headers .= "Return-Path: ".$_POST['email']."\r\n";
 
-  if($_POST['url'] == '') {
+  if($_POST['url'] == '' && $_POST['email'] != '') {
      mail($to,$subject,$message,$headers);
   }
   header("Location: ".$_POST['redirect']);
